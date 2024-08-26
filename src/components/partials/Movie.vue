5c843d4c6afe108e98ad385ff9ae7556c85251c8
Corfaya <lauracorrado0696@gmail.com>
2024-08-26 20:04:37 +0200

    [UPD] working on optimization: new file "function.js" added for functions

--- src/components/partials/Movie.vue
+++ src/components/partials/Movie.vue
@@ -1,39 +1,11 @@
 <script>
+import { getFlag } from '../../data/functions';
 export default {
     props: {
         film: Object
     },
     methods: {
-        getFlag(lang) {
-            switch (lang) {
-                case "it":
-                    return "fi fi-it"
-                case "en":
-                    return "fi fi-gb"
-                case "us":
-                    return "fi fi-us"
-                case "fr":
-                    return "fi fi-fr"
-                case "es":
-                    return "fi fi-es"
-                case "pt":
-                    return "fi fi-pt"
-                case "ja":
-                    return "fi fi-jp"
-                case "nl":
-                    return "fi fi-nl"
-                case "de":
-                    return "fi fi-de"
-                case "zh":
-                    return "fi fi-cn"
-                case "ru":
-                    return "fi fi-ru"
-                case "ko":
-                    return "fi fi-kr"
-                default:
-                    return "unknown"
-            }
-        }
+        getFlag
     },
 }
 </script>
